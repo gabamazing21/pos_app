760d9658814fe673de504297f8a904852b421249
Toba <gabeamazing21@gmail.com>
2021-01-04 10:03:23 -0500

    here

--- lib/UI/printing.dart
+++ lib/UI/printing.dart
@@ -81,12 +81,108 @@ class _MyPrintState extends State<MyPrint> {
           return pw.Center(
             child: pw.Column(children: [
               pw.Container(
+                height: 100,
+                width: 100,
                 child: pw.Image.provider(image),
               ),
+              pw.SizedBox(height: 15),
+              pw.Text(
+                'POLLO CAMPELL\'s ',
+                textAlign: pw.TextAlign.left,
+                style:
+                    pw.TextStyle(fontSize: 20, fontWeight: pw.FontWeight.bold),
+              ),
+              pw.SizedBox(height: 10),
+              pw.Container(
+                height: 20,
+                child: pw.Stack(
+                  children: [
+                    pw.Positioned(
+                      left: 10,
+                      top: 5,
+                      child: pw.Text('Guayaquil y rocafuerte'),
+                    ),
+                    pw.Positioned(
+                      right: 10,
+                      top: 5,
+                      //Get Date from Item
+                      child: pw.Text(
+                          '${itemPrinting.orderCreation.toDate().toLocal().toString().split(" ")[0]}'),
+                    )
+                  ],
+                ),
+              ),
+              pw.Container(
+                height: 40,
+                child: pw.Stack(
+                  //mainAxisAlignment: pw.MainAxisAlignment.spaceEvenly,
+                  children: [
+                    pw.Positioned(
+                        left: 10,
+                        top: 4,
+                        child: pw.Text('Milagro 00121',
+                            textAlign: pw.TextAlign.left)),
+                    pw.Positioned(
+                        right: 10,
+                        top: 4,
+                        //Get time from item
+                        child: pw.Text(
+                            '${itemPrinting.orderCreation.toDate().toLocal().toString().split(" ")[1].split(".")[0]}',
+                            textAlign: pw.TextAlign.right)),
+                    pw.Positioned(
+                      left: 10,
+                      top: 20,
+                      child: pw.Text('593990012734'),
+                    )
+                  ],
+                ),
+              ),
+              pw.Text('PURCHASE'),
+
+              pw.Divider(
+                thickness: 2,
+                height: 10,
+                color: PdfColors.black,
+              ),
+              pw.SizedBox(height: 10),
+
+              //Get Ticket From item
+
+              pw.Container(
+                height: 30,
+                child: pw.Stack(children: [
+                  pw.Positioned(
+                    left: 10,
+                    child: pw.Text('Ticket: #63'),
+                  ),
+                  pw.Positioned(
+                    left: 10,
+                    top: 12,
+                    child: pw.Text('Receipt TrmM'),
+                  )
+                ]),
+              ),
+
+              pw.Divider(
+                thickness: 1,
+                height: 10,
+                color: PdfColors.black,
+              ),
+              pw.SizedBox(height: 10),
+
               pw.ListView.builder(
+                  padding: pw.EdgeInsets.all(0),
                   itemBuilder: (pw.Context context, int index) =>
                       _printItem(itemPrinting.product.elementAt(index)),
                   itemCount: itemPrinting.product.length),
+
+              pw.Divider(
+                thickness: 1,
+                height: 10,
+                color: PdfColors.black,
+              ),
+              pw.SizedBox(height: 10),
+
               pw.Row(
                 children: [
                   pw.Text('Total Price  ='),
@@ -110,15 +206,17 @@ class _MyPrintState extends State<MyPrint> {
   }
 
   pw.Widget _printItem(Orderitem orderitem) {
-    return pw.Row(children: [
-      pw.Text(orderitem.orderItemname),
-      pw.SizedBox(width: 10),
-      pw.Text('x'),
-      pw.SizedBox(width: 10),
-      pw.Text('${orderitem.quantity}'),
-      pw.SizedBox(width: 10),
-      pw.Text('price'),
-    ]);
+    return pw.Row(
+        mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+        children: [
+          pw.Container(
+              //  width: MediaQuery.of(context).size.width * 0.2,
+              child: pw.Text(orderitem.orderItemname)),
+          pw.SizedBox(width: 10),
+          pw.Text('x ${orderitem.quantity}'),
+          pw.SizedBox(width: 10),
+          pw.Text('price'),
+        ]);
   }
 
   Future<File> getImagefromFile(String path) {
--- lib/UI/splashScreen.dart
+++ lib/UI/splashScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:passcode_screen/circle.dart';
 import 'package:passcode_screen/keyboard.dart';
 import 'package:passcode_screen/passcode_screen.dart';
+import 'package:pos_app/UI/MasterPanel.dart';
 import 'package:pos_app/UI/pass_code.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,12 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MasterPanel()),
+      );
+    });
     return Center(
         child: Container(
       child: Image.asset(
